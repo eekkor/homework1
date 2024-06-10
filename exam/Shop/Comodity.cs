0becfcb76d1ae0a36bcdd17bed59f29e06c9e993
eekkor <114683247+eekkor@users.noreply.github.com>
2024-06-10 12:50:10 +0500

    task exam done

---
+++ exam/Shop/Comodity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopLibrary
+{
+    public class Comodity
+    {
+        public string Category { get; }
+        public string Name { get; }
+        public int Price { get;}
+
+        public Comodity(string category, string name, int price)
+        {
+            Category = category;
+            Name = name;
+            Price = price;
+        }
+
+        public static Comodity Parse(string record)
+        {
+            var parts = record.Split('\t');
+            if (parts.Length != 3)
+                throw new FormatException();
+            
+
+            string category = parts[0];
+            string name = parts[1];
+            int price;
+            if (!int.TryParse(parts[2], out price))
+                throw new FormatException();
+            
+
+            return new Comodity(category, name, price);
+        }
+
+        public override string ToString() => $"{Category}\t{Name}\t{Price}";
+        
+
+        public static void Main(string[] args)
+        {
+        }
+
+    }
+}
---
+++ exam/Shop/PriceList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShopLibrary 
+{
+    public class PriceList : IPriceList 
+    {
+        private List<Comodity> list;
+
+        public PriceList(string[] records)
+        {
+            list = new List<Comodity>();
+            foreach (var record in records)
+            {
+                var comodity = Comodity.Parse(record);
+                list.Add(comodity);
+            }
+        }
+
+        public PriceList()
+        {
+            list = new List<Comodity>();
+        }
+
+        public Comodity Get(string category, string name)
+        {
+            return list.FirstOrDefault(c => c.Category.Equals(category, StringComparison.OrdinalIgnoreCase) &&
+                                            c.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public List<Comodity> GetMany(string category, string name)
+        {
+            var query = list.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(category))
+                query = query.Where(c => c.Category.IndexOf(category, StringComparison.OrdinalIgnoreCase) >= 0);
+            
+
+            if (!string.IsNullOrWhiteSpace(name))
+                query = query.Where(c => c.Name.IndexOf(name, StringComparison.OrdinalIgnoreCase) >= 0);
+
+            return query.ToList();
+        }
+    }
+}
