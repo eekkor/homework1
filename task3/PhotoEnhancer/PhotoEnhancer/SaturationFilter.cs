cf5e8b2c21a2f93c12753ae573640882a103f2ac
eekkor <114683247+eekkor@users.noreply.github.com>
2024-06-10 19:32:48 +0500

    task-3

---
+++ task3/PhotoEnhancer/PhotoEnhancer/SaturationFilter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using PhotoEnhancer;
+
+namespace PhotoEnhancer.Filters.Transformations
+{
+    public class SaturationParameters
+    {
+        public double Coefficient { get; set; }
+    }
+
+    public class SaturationFilter : PixelFilter<SaturationParameters>
+    {
+        public SaturationFilter() : base("Насыщенность",
+            (pixel, parameters) =>
+            {
+                var hsl = Convertors.RGBToHSL(pixel);
+                hsl.S = hsl.S * parameters.Coefficient;
+                hsl.S = Math.Clamp(hsl.S, 0, 1);
+                var newPixel = Convertors.HSLToRGB(hsl);
+                return new Pixel(
+                    Math.Clamp(newPixel.R, 0, 1),
+                    Math.Clamp(newPixel.G, 0, 1),
+                    Math.Clamp(newPixel.B, 0, 1));
+            })
+        { }
+    }
+}
